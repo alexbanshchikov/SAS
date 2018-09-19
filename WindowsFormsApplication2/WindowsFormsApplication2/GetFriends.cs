a128efebd87607ab0a893cef5397041473971265
AGirutskaya <Giruch@ro.ru>
2018-09-19 13:46:33 +0700

    Start

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/GetFriends.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+using Newtonsoft;
+using VkNet.Model.RequestParams;
+
+namespace WindowsFormsApplication2
+{
+    internal class GetFriends
+    {
+        private readonly Form1 form;
+        private VkApi vk;
+        private List<User> user;
+        private double[,] graf;
+
+        public GetFriends(Form1 form)
+        {
+            this.form = form;
+        }
+
+        public void Avtoriz()
+        {
+            vk = new VkApi();
+
+            var login = form.textBox1.Text;
+            var pass = form.textBox2.Text;
+            ulong id = 6285595;
+            Settings setting = Settings.All;
+            vk.Authorize(new ApiAuthParams
+            {
+                Login = login,
+                Password = pass,
+                ApplicationId = id,
+                Settings = setting,
+            });
+        }
+
+        public void Get_Friend_Information()
+        {
+            user = new List<User>();
+            var friend = vk.Friends.Get(75234556, ProfileFields.FirstName | ProfileFields.LastName, 20).ToArray();
+            for (var i = 0; i < friend.Length - 1; i++)
+            {
+                if (friend[i].Deactivated == null)
+                {
+                    var follows = vk.Users.GetFollowers(friend[i].Id, 1000);
+                    var friend_2 = vk.Friends.Get(friend[i].Id);
+
+                    User u = new User();
+                    u.Id_friend = friend[i].Id;
+                    u.Num_followers = follows.Count;
+                    u.Num_friend = friend_2.Count;
+                    u.Lastname = friend[i].LastName;
+                    u.Observation = new double[2];
+                    u.Observation[0] = (double)u.Num_friend;
+                    u.Observation[1] = (double)u.Num_followers;
+                    u.Num_clust = new int();
+                    user.Add(u);
+                }
+            }
+            foreach (User u in user)
+            {
+                form.textBox3.Text += u.Lastname + " " + u.Num_friend.ToString() + " " + u.Num_followers.ToString() + " " + Environment.NewLine;
+            }
+        }
+
+        public List<User> Get_user()
+        {
+            return user;
+        }
+
+
+        public double[,] Analysis_distance()
+        {
+            graf = new double[user.Count * user.Count, 3];
+            double i1 = 0, i2 = 0;
+            
+            form.textBox3.Text = null;
+            for (int k = 0; k < graf.GetLength(0); k++)
+            {
+                graf[k, 0] = Math.Floor(i1 / user.Count);
+                graf[k, 1] = i1 % user.Count;
+                double d1 = 0, d2 = 0;
+                d1 = Math.Pow(user[Convert.ToInt32(graf[k, 0])].Num_followers - user[Convert.ToInt32(graf[k, 1])].Num_followers, 2);
+                d2 = Math.Pow(user[Convert.ToInt32(graf[k, 0])].Num_friend - user[Convert.ToInt32(graf[k, 1])].Num_friend, 2);
+                graf[k, 2] = Math.Sqrt(d1 + d2);
+                i1++;
+
+            }
+            return graf;
+        }
+    }
+    
+
+    public class User
+    {
+
+        private string _lastname;
+        private long _idFriend;
+        private int num_followers;
+        private int num_clust;
+        private double[] observation;
+        private int num_friend;
+
+        public string Lastname
+        {
+            get
+            {
+                return _lastname;
+            }
+
+            set
+            {
+                _lastname = value;
+            }
+        }
+
+        public long Id_friend
+        {
+            get
+            {
+                return _idFriend;
+            }
+
+            set
+            {
+                _idFriend = value;
+            }
+        }
+
+        public int Num_followers
+        {
+            get
+            {
+                return num_followers;
+            }
+
+            set
+            {
+                num_followers = value;
+            }
+        }
+
+        public int Num_clust
+        {
+            get
+            {
+                return num_clust;
+            }
+
+            set
+            {
+                num_clust = value;
+            }
+        }
+
+        public double[] Observation
+        {
+            get
+            {
+                return observation;
+            }
+
+            set
+            {
+                observation = value;
+            }
+        }
+
+        public int Num_friend
+        {
+            get
+            {
+                return num_friend;
+            }
+
+            set
+            {
+                num_friend = value;
+            }
+        }
+    }
+}
+
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Prim.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Prim
+    {
+        private List<Edge> MST;
+        private List<List<int>> clusters;
+        private double[,] matr;
+        List<int> notUsed;
+
+        public void AlgorithmByPrim(int numberV, double[,] E, Form1 form, int numberClust, List<User> us)
+        {
+
+            MST = new List<Edge>();
+            clusters = new List<List<int>>();
+
+            List<Edge> notUsedE = new List<Edge>();
+            List<double> usedV = new List<double>();
+            List<double> notUsedV = new List<double>();
+
+            for (int i = 0; i < E.GetLength(0); i++)
+            {
+                Edge e = new Edge(E[i, 0], E[i, 1], E[i, 2]);
+                notUsedE.Add(e);
+
+            }
+
+            for (int i = 0; i < numberV; i++)
+                notUsedV.Add(i);
+
+            Random rand = new Random();
+            usedV.Add(rand.Next(0, numberV));
+            notUsedV.RemoveAt((int)usedV[0]);
+            while (notUsedV.Count > 0)
+            {
+                int minE = -1; //номер наименьшего ребра
+                               //поиск наименьшего ребра
+                for (int i = 0; i < notUsedE.Count; i++)
+                {
+                    if ((usedV.IndexOf(notUsedE[i].v1) != -1) && (notUsedV.IndexOf(notUsedE[i].v2) != -1) ||
+                        (usedV.IndexOf(notUsedE[i].v2) != -1) && (notUsedV.IndexOf(notUsedE[i].v1) != -1))
+                    {
+                        if (minE != -1)
+                        {
+                            if (notUsedE[i].weight < notUsedE[minE].weight)
+                                minE = i;
+                        }
+                        else
+                            minE = i;
+                    }
+                }
+                //заносим новую вершину в список использованных и удаляем ее из списка неиспользованных
+                if (usedV.IndexOf(notUsedE[minE].v1) != -1)
+                {
+                    usedV.Add(notUsedE[minE].v2);
+                    notUsedV.Remove(notUsedE[minE].v2);
+                }
+                else
+                {
+                    usedV.Add(notUsedE[minE].v1);
+                    notUsedV.Remove(notUsedE[minE].v1);
+                }
+                //заносим новое ребро в дерево и удаляем его из списка неиспользованных
+                MST.Add(notUsedE[minE]);
+                notUsedE.RemoveAt(minE);
+            }
+            Make_clusters(form,numberV,numberClust,us);
+        }
+
+        public void Make_clusters(Form1 form, int numberV, int numberClust, List<User> us)
+        {
+            matr = new double[numberV, numberV];
+            notUsed = new List<int>();
+            var spanningTree = (from t in MST
+                                 orderby t.weight descending   // упорядочиваем по возрастанию
+                                 select t).ToList();
+
+            for (int i = 0; i < numberV; i++)
+            {
+                for (int j = 0; j < numberV; j++)
+                {
+                    matr[i, j] = 0;
+                }
+            }
+            for (int i = numberClust - 1; i < spanningTree.Count; i++)
+            {
+                matr[(int)spanningTree[i].v1, (int)spanningTree[i].v2] = 1;
+                matr[(int)spanningTree[i].v2, (int)spanningTree[i].v1] = 1;
+            }
+
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                notUsed.Add(i);
+            }
+            Sch_Clusters();
+
+            form.textBox3.Text += Environment.NewLine + Environment.NewLine + "Метод Прима " + Environment.NewLine;
+            form.textBox3.Text += Environment.NewLine;
+            int numb = 1;
+            foreach (List<int> item in clusters)
+            {
+                form.textBox3.Text += "Кластер №" + numb + Environment.NewLine;
+                foreach (int g1 in item)
+                {
+                    form.textBox3.Text += us[g1].Lastname + " ";
+                }
+                numb++;
+                form.textBox3.Text += Environment.NewLine + Environment.NewLine;
+            }
+        }
+
+        public void Sch_Clusters()
+        {
+            while (notUsed.Count != 0)
+            {
+                List<int> used = new List<int>();
+                Dpt(notUsed.First(), used);
+                clusters.Add(used);          
+            }
+        }
+
+        public void Dpt(int v, List<int> used)
+        {
+            used.Add(v);
+            notUsed.Remove(v);
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                if ((matr[v, i] == 1) && (notUsed.Contains(i))){
+                    Dpt(i,used);
+                }
+            }
+        }
+    }
+
+
+
+
+        public class Edge
+        {
+            public double v1, v2;
+
+            public double weight;
+
+            public Edge(double v1, double v2, double weight)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+                this.weight = weight;
+            }
+        }
+    }
+    
+
